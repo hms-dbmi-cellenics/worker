c5794f07a5b9144fc6febd4bd8d77fc739477899
iva babukova <ibabukova@gmail.com>
2020-04-30 16:27:18 +0100

    send stuff to sns topic

--- src/work.py
+++ src/work.py
@@ -43,25 +43,29 @@ def run_task(body, adata):
         raise e
 
 
+def publish_result(result):
+    sns = boto3.client("sns")
+    # result = result.tolist()
+    result = "blablablablaba"
+    resp = sns.publish(
+        TargetArn="arn:aws:sns:eu-west-2:242905224710:work-results-staging",
+        Message=json.dumps({"default": result}),
+        Subject="a short subject for your message",
+        MessageStructure="json",
+    )
+    print(resp)
+
+
 def main():
     """
     Consumes and parses instructions from a message queue.
-
-    It takes the following environment variables: WORK_QUEUE (the SQS queue
-    to join), WORK_TIMEOUT (the timeout for this session in
-    seconds, default: 60*20).
     """
-    queue = os.getenv("WORK_QUEUE", "test-queue")
-
-    if not queue:
-        raise ValueError("No work queue specified.")
-
     sqs = boto3.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
-    print(f"Now listening, waiting for work to do...")
-
     last_activity = datetime.datetime.now()
     adata = None
+
+    print(f"Now listening, waiting for work to do...")
 
     while (datetime.datetime.now() - last_activity).total_seconds() <= TIMEOUT:
         message = queue.receive_messages(WaitTimeSeconds=20)
@@ -69,10 +73,9 @@ def main():
         if not message:
             continue
 
-        message = message[0]
-
         # Try to parse it as JSON
         try:
+            message = message[0]
             body = json.loads(message.body)
         except Exception as e:
             print("Exception when loading json: ", e)
@@ -90,6 +93,7 @@ def main():
 
         # Publish task results to SNS topic
         print(result)
+        publish_result(result)
 
         last_activity = datetime.datetime.now()
 
