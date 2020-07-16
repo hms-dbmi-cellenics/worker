c7303ac95ea06233414fdfc84d6f049d1759d12f
Marcell Pek <marcell.pek@marcellp.net>
2020-07-16 10:51:19 +0100

    fix

---
+++ src/tasks/gene_expression_new.py
@@ -0,0 +1,68 @@
+import json
+import numpy as np
+from config import get_config
+from result import Result
+
+config = get_config()
+
+
+class GeneExpressionNew:
+    def __init__(self, msg, adata):
+        self.adata = adata
+        self.task_def = msg["body"]
+        self.experiment_id = msg["experimentId"]
+
+    def _format_result(self, result):
+        # JSONify result.
+        result = json.dumps(result)
+
+        # Return a list of formatted results.
+        return [Result(result)]
+
+    def compute(self):
+        # the genes to get expression data for
+        genes = self.task_def["genes"]
+
+        # whether to perform feature scaling (defaults to False)
+        scale = self.task_def.get("scale", False)
+
+        # compute data on raw matrix
+        raw_adata = self.adata.raw.to_adata()
+        raw_adata = raw_adata.copy()
+        raw_adata.X = raw_adata.X.toarray()
+
+        # create a proper ordering of cells by increasing IDs
+        obs_copy = raw_adata.obs.copy()
+        obs_copy.sort_values(by=["cell_ids"], inplace=True)
+        obs_copy = obs_copy.dropna()
+        cell_list = obs_copy.index.tolist()
+
+        # try to find all genes in the list
+        raw_adata.var["genes_to_compute"] = raw_adata.var.index.isin(genes)
+
+        # this orders and filters the matrix correctly for both cells and genes
+        raw_adata = raw_adata[
+            cell_list, raw_adata.var["genes_to_compute"],
+        ]
+
+        # if feature scaling is desired, perform that now
+        # we do not do this now.
+        if scale:
+            pass
+
+        result = {}
+
+        for gene in genes:
+            view = raw_adata[:, raw_adata.var.index == gene]
+
+            expression = view.X.flatten().tolist()
+            minimum = float(np.amin(view.X))
+            maximum = float(np.amax(view.X))
+
+            result[gene] = {
+                "min": minimum,
+                "max": maximum,
+                "expression": expression,
+            }
+
+        return self._format_result(result)
---
+++ src/tests/tasks/test_gene_expression_new.py
@@ -0,0 +1,75 @@
+import pytest
+import anndata
+import os
+from tasks.gene_expression_new import GeneExpressionNew
+import json
+from botocore.stub import Stubber
+import mock
+import boto3
+from config import get_config
+from boto3.dynamodb.types import TypeSerializer
+
+config = get_config()
+
+
+class TestGeneExpressionNew:
+    @pytest.fixture(autouse=True)
+    def open_test_adata(self):
+        self._adata = anndata.read_h5ad(os.path.join("tests", "test.h5ad"))
+
+    @pytest.fixture(autouse=True)
+    def load_correct_definition(self):
+        self.correct_request = {
+            "experimentId": "5e959f9c9f4b120771249001",
+            "timeout": "2099-12-31 00:00:00",
+            "body": {"name": "GeneExpressionNew", "genes": ["TGFB1", "CST3"],},
+        }
+
+    def test_throws_on_missing_parameters(self):
+        with pytest.raises(TypeError):
+            GeneExpressionNew()
+
+    def test_throws_on_missing_adata(self):
+        with pytest.raises(TypeError):
+            GeneExpressionNew(self.correct_request)
+
+    def test_works_with_request_and_adata(self):
+        GeneExpressionNew(self.correct_request, self._adata)
+
+    def test_returns_json(self):
+        res = GeneExpressionNew(self.correct_request, self._adata).compute()
+        res = res[0].result
+        json.loads(res)
+
+    def test_returns_a_json_object(self):
+        res = GeneExpressionNew(self.correct_request, self._adata).compute()
+        res = res[0].result
+        res = json.loads(res)
+        assert isinstance(res, dict)
+
+    def test_object_returns_appropriate_number_of_genes(self):
+        res = GeneExpressionNew(self.correct_request, self._adata).compute()
+        res = res[0].result
+        res = json.loads(res)
+
+        assert len(res) == len(self.correct_request["body"]["genes"])
+
+    def test_each_expression_data_has_correct_number_of_cells(self):
+        res = GeneExpressionNew(self.correct_request, self._adata).compute()
+        res = res[0].result
+        res = json.loads(res)
+
+        for v in res.values():
+            assert len(v["expression"]) == len(self._adata.obs)
+
+    def test_min_and_max_expression_data_gets_displayed_appropriately(self):
+        res = GeneExpressionNew(self.correct_request, self._adata).compute()
+        res = res[0].result
+        res = json.loads(res)
+
+        for v in res.values():
+            minimum = v["min"]
+            maximum = v["max"]
+
+            assert minimum == min(v["expression"])
+            assert maximum == max(v["expression"])
