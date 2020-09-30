2efc1b53a42fb819ab4cbbfeb5837186716cb673
Marcell Pek <marcell.pek@marcellp.net>
2020-09-30 13:54:52 +0100

    fix

--- src/tasks/gene_expression.py
+++ src/tasks/gene_expression.py
@@ -23,37 +23,39 @@ class GeneExpression:
         # the genes to get expression data for
         genes = self.task_def["genes"]
 
-        # whether to perform feature scaling (defaults to False)
-        scale = self.task_def.get("scale", False)
+        # whether to perform feature scaling (defaults to True)
+        scale = self.task_def.get("scale", True)
+
+        if scale:
+            correct_adata = self.adata.copy()
+        else:
+            correct_adata = self.adata.raw.to_adata()
+            correct_adata = correct_adata.copy()
 
         # compute data on raw matrix
-        raw_adata = self.adata.raw.to_adata()
-        raw_adata = raw_adata.copy()
-        raw_adata.X = raw_adata.X.toarray()
+        correct_adata = self.adata.raw.to_adata()
+        correct_adata = correct_adata.copy()
+        correct_adata.X = correct_adata.X.toarray()
 
         # create a proper ordering of cells by increasing IDs
-        obs_copy = raw_adata.obs.copy()
+        obs_copy = correct_adata.obs.copy()
         obs_copy.sort_values(by=["cell_ids"], inplace=True)
         obs_copy = obs_copy.dropna()
         cell_list = obs_copy.index.tolist()
 
         # try to find all genes in the list
-        raw_adata.var["genes_to_compute"] = raw_adata.var.index.isin(genes)
+        correct_adata.var["genes_to_compute"] = correct_adata.var.index.isin(genes)
 
         # this orders and filters the matrix correctly for both cells and genes
-        raw_adata = raw_adata[
-            cell_list, raw_adata.var["genes_to_compute"],
+        correct_adata = correct_adata[
+            cell_list,
+            correct_adata.var["genes_to_compute"],
         ]
-
-        # if feature scaling is desired, perform that now
-        # we do not do this now.
-        if scale:
-            pass
 
         result = {}
 
-        for gene in raw_adata.var.index:
-            view = raw_adata[:, raw_adata.var.index == gene]
+        for gene in correct_adata.var.index:
+            view = correct_adata[:, correct_adata.var.index == gene]
 
             expression = view.X.flatten().tolist()
             minimum = float(np.amin(view.X))
