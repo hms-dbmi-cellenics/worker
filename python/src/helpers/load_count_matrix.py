3bdd4445262e106bbb8d022e84ca83f5df8e4405
Marcell Pek <marcell.pek@marcellp.net>
2020-10-08 16:15:34 +0100

    fix

--- python/src/helpers/load_count_matrix.py
+++ python/src/helpers/load_count_matrix.py
@@ -2,44 +2,32 @@ from config import get_config
 import boto3
 import datetime
 import anndata
-import os
-import tempfile
+import io
 from helpers.dynamo import get_item_from_dynamo
 
 config = get_config()
 
 
 def _download_obj(bucket, key):
-
-    tmpdir = tempfile.TemporaryDirectory()
-
-    if config.CLUSTER_ENV == "development" or config.CLUSTER_ENV == "test":
-        print("In development, creating a temporary directory...")
-        path = tmpdir.name
-    else:
-        path = "/data"
-
     try:
         client = boto3.client("s3", **config.BOTO_RESOURCE_KWARGS)
-        file_path = os.path.join(path, f"{bucket}_{key}")
-
-        with open(file_path, "wb") as f:
-            client.download_fileobj(Bucket=bucket, Key=key, Fileobj=f)
-
-        adata = anndata.read_h5ad(file_path)
-        print(datetime.datetime.utcnow(), "File read.")
-        return adata
-
+        result = io.BytesIO()
+        client.download_fileobj(Bucket=bucket, Key=key, Fileobj=result)
+        result.seek(0)
     except Exception as e:
         print(datetime.datetime.utcnow(), "Could not get file from S3", e)
         raise e
+    print(datetime.datetime.utcnow(), "File was loaded.")
+    return result
 
 
 def _load_file(matrix_path):
     # intercept here if task is to prepare experiment, don't download from s3
     print(datetime.datetime.utcnow(), "Have to download anndata file from s3")
     bucket, key = matrix_path.split("/", 1)
-    adata = _download_obj(bucket, key)
+    result = _download_obj(bucket, key)
+    adata = anndata.read_h5ad(result)
+
     print(datetime.datetime.utcnow(), "File was loaded.")
 
     if "cell_ids" not in adata.obs:
