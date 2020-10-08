537635a5dbeba67522bf2a9b389570565584603d
Marcell Pek <marcell.pek@marcellp.net>
2020-10-08 15:36:03 +0100

    fix

--- python/src/helpers/r_readiness.py
+++ python/src/helpers/r_readiness.py
@@ -6,14 +6,19 @@ from config import get_config
 config = get_config()
 
 
-def fatal_error(e):
-    if e.response and 400 <= e.response.status_code:
-        return True
-
-
-@backoff.on_exception(backoff.expo, Exception, max_time=20, giveup=fatal_error)
+@backoff.on_exception(
+    backoff.expo,
+    Exception,
+    max_time=20,
+    giveup=lambda e: e.response and e.response.status_code >= 400,
+)
 def check_r_readiness():
     print(datetime.datetime.utcnow(), "Attempting to connect to R worker...")
 
     r = requests.head(f"{config.R_WORKER_URL}/health")
     r.raise_for_status()
+
+    print(
+        datetime.datetime.utcnow(),
+        f"Successful connection, status code {r.status_code}.",
+    )
