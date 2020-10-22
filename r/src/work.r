0fce9a15dd4a7473efbdf948484186adb1e27e13
Marcell Pek <marcell.pek@marcellp.net>
2020-10-22 18:06:30 +0100

    fix

--- r/src/work.r
+++ r/src/work.r
@@ -6,60 +6,96 @@ require(data.table)
 library(DESeq2)
 library(RJSONIO)
 
-experiment_id <- Sys.getenv("EXPERIMENT_ID")
-message(paste("Welcome to Biomage R worker, experiment id", experiment_id))
+load_data <- function() {
+    experiment_id <- Sys.getenv("EXPERIMENT_ID")
+    message(paste("Welcome to Biomage R worker, experiment id", experiment_id))
 
-data <- readRDS(paste("/data", experiment_id, "r.rds", sep = "/"))
-length <- dim(data$counts)
+    loaded <- F
+    data <- NULL
 
-message(
-    paste("Data successfully loaded, dimensions", length[1], "x", length[2])
-)
+    while (!loaded) {
+        data <- tryCatch(
+            {
+                f <- readRDS(
+                    paste(
+                        "/data", "5e959f9c9f4b120771249001", "rb.rds",
+                        sep = "/"
+                    )
+                )
 
-app <- Application$new(content_type = "application/json")
+                loaded <- T
+                length <- dim(f$counts)
 
-app$add_get(
-    path = "/health",
-    FUN = function(request, response) {
-        response$set_body("up")
+                message(
+                    paste(
+                        "Data successfully loaded, dimensions",
+                        length[1], "x", length[2]
+                    )
+                )
+
+                f
+            },
+            warning = function(w) {
+                message(paste("file could not be loaded: ", w))
+            },
+            error = function(e) {
+                message(paste("file could not be loaded: ", e))
+            }
+        )
+        Sys.sleep(1)
     }
-)
 
-app$add_post(
-    path = "/v0/DifferentialExpression",
-    FUN = function(req, res) {
+    return(data)
+}
 
-        # set up a factor with the appropriate cells
-        factor <- rep(
-            c("base", "background"),
-            c(
-                length(req$body$baseCells),
-                length(req$body$backgroundCells)
+create_app <- function(data) {
+    app <- Application$new(content_type = "application/json")
+
+    app$add_get(
+        path = "/health",
+        FUN = function(request, response) {
+            response$set_body("up")
+        }
+    )
+
+    app$add_post(
+        path = "/v0/DifferentialExpression",
+        FUN = function(req, res) {
+
+            # set up a factor with the appropriate cells
+            factor <- rep(
+                c("base", "background"),
+                c(
+                    length(req$body$baseCells),
+                    length(req$body$backgroundCells)
+                )
             )
-        )
 
-        names(factor) <- c(
-            req$body$baseCells, req$body$backgroundCells
-        )
+            names(factor) <- c(
+                req$body$baseCells, req$body$backgroundCells
+            )
 
-        data$clusters$custom$de <- factor(factor)
+            data$clusters$custom$de <- factor(factor)
 
-        # # compute differential expression
-        data$getDifferentialGenes(
-            type = "custom", verbose = T, clusterType = "de"
-        )
+            # # compute differential expression
+            data$getDifferentialGenes(
+                type = "custom", verbose = T, clusterType = "de"
+            )
 
-        # # get the necessary results
-        result <- data$diffgenes$custom$de$base
+            # # get the necessary results
+            result <- data$diffgenes$custom$de$base
 
-        # replace name with Gene names
-        result$Gene <- data$misc$gene_annotations[
-            match(result$Gene, data$misc$gene_annotations$input), "name"
-        ]
+            # replace name with Gene names
+            result$Gene <- data$misc$gene_annotations[
+                match(result$Gene, data$misc$gene_annotations$input), "name"
+            ]
 
-        res$set_body(result)
-    }
-)
+            res$set_body(result)
+        }
+    )
+}
 
+data <- load_data()
 backend <- BackendRserve$new()
-backend$start(app, http_port = 4000)
+
+backend$start(create_app(data), http_port = 4000)
