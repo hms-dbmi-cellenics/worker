70e2b7b9af481ee6ff88e89cc4d9723106e89124
Marcell Pek <marcell.pek@marcellp.net>
2020-10-23 13:31:44 +0100

    fix

--- python/src/tests/tasks/test_differential_expression.py
+++ python/src/tests/tasks/test_differential_expression.py
@@ -169,7 +169,7 @@ class TestDifferentialExpression:
 
         for row in res["rows"]:
             keys = sorted(row.keys())
-            expected_keys = sorted(["gene_names", "zscore", "abszscore", "log2fc"])
+            expected_keys = sorted(["gene_names", "zscore", "abszscore", "log2fc", "_row"])
             assert keys == expected_keys
 
     @responses.activate
