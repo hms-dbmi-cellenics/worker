16f604ae62b993c4c5de84ee4ad92fe13199e94f
Juanlu <juanluonieva@uma.es>
2021-02-09 18:18:50 +0100

    Change ID for testing

--- python/src/tests/tasks/test_cluster_cells.py
+++ python/src/tests/tasks/test_cluster_cells.py
@@ -19,7 +19,7 @@ class TestClusterCells:
     @pytest.fixture(autouse=True)
     def load_correct_definition(self):
         self.correct_request = {
-            "experimentId": "5e959f9c9f4b120771249001",
+            "experimentId": "5928a56c7cbff9de78974ab50765ed20",
             "timeout": "2099-12-31 00:00:00",
             "body": {
                 "name": "ClusterCells",
@@ -49,7 +49,7 @@ class TestClusterCells:
 
     def test_leiden_clustering_works(self):
         alternative_request = {
-            "experimentId": "5e959f9c9f4b120771249001",
+            "experimentId": "5928a56c7cbff9de78974ab50765ed20",
             "timeout": "2099-12-31 00:00:00",
             "body": {
                 "name": "ClusterCells",
