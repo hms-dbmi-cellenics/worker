764674a1bef101a3617328bfda2c397d925f3f40
ogibson <oliver@biomage.net>
2021-02-10 16:21:53 -0300

    added comment

--- r/src/expression.r
+++ r/src/expression.r
@@ -5,6 +5,9 @@
 # req$body has
 # genes: list of gene common names to search for in the annotation.  
 #
+#
+#For now we return the values stored in data (normalized values). When the correct config parameter is set on the UI, we'll add the scaled values. 
+#
 runExpression <- function(req) {
     #Get the annotation matrix with the geneid to name translation, and the subset with the correct names.
     df <- data@misc$gene_annotations
