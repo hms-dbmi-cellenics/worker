9273e2b2597c24d88303e800bb93c2c820ea7cd0
ogibson-biomage <76957896+ogibson-biomage@users.noreply.github.com>
2021-02-24 10:38:54 -0300

    Cluster ge (#73)
    * Cluster cells worker task on python, first commit
    
    * Added cluster.r file

--- r/src/work.r
+++ r/src/work.r
@@ -10,6 +10,7 @@ source("./differential_expression.r")
 source("./embedding.r")
 source("./get_metadata_information.r")
 source("./expression.r")
+source("./cluster.r")
 
 load_data <- function() {
     experiment_id <- Sys.getenv("EXPERIMENT_ID", unset = "5928a56c7cbff9de78974ab50765ed20")
@@ -98,7 +99,14 @@ create_app <- function(data) {
             res$set_body(result)
     	}
     )
-  
+    app$add_post(
+        path = "/v0/getClusters",
+        FUN = function(req, res) {
+            result <- getClusters(req)
+            res$set_body(result)
+    	}
+    )
+
     return(app)
 }
 
