c58ebf0e1a4c2bb805ac620294445a6dc72305bc
ogibson <oliver@biomage.net>
2021-04-12 15:41:28 -0300

    Added fix for heatmap

--- r/src/expression.r
+++ r/src/expression.r
@@ -12,37 +12,18 @@ runExpression <- function(req) {
     #Get the annotation matrix with the geneid to name translation, and the subset with the correct names.
     df <- data@misc$gene_annotations
     genesSubset <- subset(df, toupper(df$name) %in% toupper(req$body$genes))
+    #
     #Get the expression values for those genes in the corresponding matrix.
-    geneExpression <- as.matrix(data@assays$RNA@data[genesSubset$input,])
-    #
-    #There might be a better way to do this, but when the count is 1 the previous
-    #search returns a transposed matrix without the gene id corresponding to that only column
-    #so I had to fix that.
-    #
-    if (ncol(geneExpression)==1){
-        geneExpression<-t(geneExpression)
-        for(gene in genesSubset$input){
-            #
-            #data@assays$RNA@data
-            if (gene %in% rownames(data@assays$RNA@data)){
-                rownames(geneExpression)<-gene  
-                break
-            }    
-        }
-    }
-    #
-    #Now I need to get the gene name from the IDs of the ones that were found on the data matrix.
-    #With those values, replace the rownames. If I had used the whole genesSubset$input as gene names
-    #I'd have risked adding a column for a gene that wasn't found on the data matrix.
-    #
-    #Another possible way of doing this is:
-    #merge(genesSubset,geneExpression,by.x="input",by.y=0,all.y=TRUE,all.x=FALSE)
-    #I don't know which one is faster, for now I'll leave this one.
-    #
-    #Juanlu suggested genesSubset$name[match(rownames(geneExpression), genesSubset$input)]
-    #
-    rownames(geneExpression) <- genesSubset$name[match(rownames(geneExpression), genesSubset$input)]
-    geneExpression <- as.list(as.data.frame(t(as.matrix(geneExpression))))
+    geneExpression <-data@assays$RNA@data[genesSubset$input,,drop=FALSE]
+    geneExpression <- as.data.frame(t(as.matrix(geneExpression)))
+    geneExpression$cells_id <- data@meta.data$cells_id
+    geneExpression <- geneExpression[ order(geneExpression$cells_id), ]
+    geneExpression <- geneExpression %>%
+        complete(cells_id = seq(0,max(data@meta.data$cells_id))) %>%
+        select(-cells_id)
+    # worried about duplicate gene row.names in @data
+    symbol_idx <- match(colnames(geneExpression), genesSubset$input)
+    colnames(geneExpression) <- genesSubset$name[symbol_idx]
+    #geneExpression <- as.list(as.data.frame(t(as.matrix(geneExpression))))
     return(geneExpression)
 }
-
