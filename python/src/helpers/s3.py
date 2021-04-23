2ac7a063b40430a65b4c01801c8a3d9737925862
Martin Fosco <martinfosco@192.168.1.9>
2021-04-22 21:09:48 -0300

    Fix pathing issues

--- python/src/helpers/s3.py
+++ python/src/helpers/s3.py
@@ -7,9 +7,16 @@ from helpers.dynamo import get_item_from_dynamo
 
 config = get_config()
 
+
 def get_cell_sets(experiment_id):
     path = os.path.join(config.LOCAL_DIR, f"{experiment_id}/cell_sets.json")
-    with open(path, 'wb+') as f:
+
+    path_exists = os.path.exists(path)
+
+    if not path_exists:
+        os.makedirs(path)
+
+    with open(path, "wb+") as f:
         s3 = boto3.client("s3", **config.BOTO_RESOURCE_KWARGS)
 
         print("downloading cellsets")
@@ -21,8 +28,9 @@ def get_cell_sets(experiment_id):
         )
 
         f.seek(0)
-        
+
         cell_sets_string = f.read()
+
         cell_sets = json.loads(cell_sets_string)
-                
+
         return cell_sets["cellSets"]
