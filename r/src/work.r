52cb31678872f81d96f41a2e69c781501dd1ee0a
Alex Pickering <alexvpickering@gmail.com>
2021-05-18 14:26:07 -0700

    Init debug (#107)
    * init debug for worker
    
    * use ./data/debug for debug_path
    
    * fix typo
    
    * Update README.md
    
    Co-authored-by: seb-mueller <sebm@posteo.de>
    
    * add Sebastian's suggestions
    
    * update README
    
    * revert docker-compose
    
    Co-authored-by: seb-mueller <sebm@posteo.de>

--- r/src/work.r
+++ r/src/work.r
@@ -14,6 +14,10 @@ source("./cluster.r")
 
 experiment_id <- Sys.getenv("EXPERIMENT_ID", unset = "e52b39624588791a7889e39c617f669e")
 
+debug_step <- Sys.getenv("DEBUG_STEP", unset = "")
+# over-ride manually to hot-reload
+# debug_step <- "getClusters"
+
 load_data <- function() {
     message(paste("Welcome to Biomage R worker, experiment id", experiment_id))
 
@@ -57,7 +61,38 @@ load_data <- function() {
     return(data)
 }
 
-create_app <- function(last_modified) {    
+run_post <- function(req, post_fun) {
+    handle_debug(req)
+    post_fun(req)
+}
+
+handle_debug <- function(req) {
+    task_name <- basename(req$path)
+    is_debug <- debug_step == task_name | debug_step == 'all'
+
+    if (is_debug) {
+        message(sprintf('⚠ DEBUG_STEP = %s. Saving `req` object.', task_name))
+        req_fname <- sprintf('%s_%s_req.rds', experiment_id, task_name)
+        saveRDS(req, file.path('/debug', req_fname))
+
+        req_host  <- file.path('./data/debug', req_fname)
+        message(sprintf("⚠ RUN req  <- readRDS('%s') to restore 'req' object.",  req_host))
+
+        # copy data to /debug if doesn't exist
+        data_fname <- sprintf('%s_data.rds', experiment_id)
+        data_cont <- file.path('/debug', data_fname)
+
+        if (!file.exists(data_cont)) {
+            data_path <- file.path('/data', experiment_id, 'r.rds')
+            file.copy(data_path, data_cont)
+        }
+
+        data_host <- file.path('./data/debug', data_fname)
+        message(sprintf("⚠ RUN data <- readRDS('%s') to restore 'data' object.", data_host))
+    }
+}
+
+create_app <- function(last_modified) {
     last_modified_mw <- Middleware$new(
         process_request = function(request, response) {
             if (!file.info(path)$mtime == last_modified) {
@@ -88,42 +123,42 @@ create_app <- function(last_modified) {
     app$add_post(
         path = "/v0/DifferentialExpression",
         FUN = function(req, res) {
-            result <- runDE(req)
+            result <- run_post(req, runDE)
             res$set_body(result)
         }
     )
     app$add_post(
         path = "/v0/getEmbedding",
         FUN = function(req, res) {
-            result <- runEmbedding(req)
+            result <- run_post(req, runEmbedding)
             res$set_body(result)
         }
     )
     app$add_post(
         path = "/v0/getDoubletScore",
         FUN = function(req, res) {
-            result <- getDoubletScore(req)
+            result <- run_post(req, getDoubletScore)
             res$set_body(result)
         }
     )
     app$add_post(
         path = "/v0/getMitochondrialContent",
         FUN = function(req, res) {
-            result <- getMitochondrialContent(req)
+            result <- run_post(req, getMitochondrialContent)
             res$set_body(result)
         }
     )
     app$add_post(
         path = "/v0/getExpression",
         FUN = function(req, res) {
-            result <- runExpression(req)
+            result <- run_post(req, runExpression)
             res$set_body(result)
     	}
     )
     app$add_post(
         path = "/v0/listGenes",
         FUN = function(req, res) {
-            result <- getList(req)
+            result <- run_post(req, getList)
             res$set_body(result)
     	}
     )
@@ -131,7 +166,7 @@ create_app <- function(last_modified) {
         path = "/v0/getClusters",
         FUN = function(req, res) {
             str(req$body)
-            result <- getClusters(req)
+            result <- run_post(req, getClusters)
             res$set_body(result)
     	}
     )
