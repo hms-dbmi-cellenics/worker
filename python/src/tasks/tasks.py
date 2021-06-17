c1a6d82606e7e05c554752d77c95bb24df13d0d0
James Robinson <robioson@pm.me>
2021-06-17 16:08:10 +0100

    Simplify TaskFactory

--- python/src/tasks/tasks.py
+++ python/src/tasks/tasks.py
@@ -1,13 +1,15 @@
 import traceback
 import json
+
 from .doublet_score import GetDoubletScore
 from .mitochondrial_content import GetMitochondrialContent
-from .embedding import ComputeEmbedding
+from .embedding import GetEmbedding
 from .list_genes import ListGenes
 from .differential_expression import DifferentialExpression
 from .gene_expression import GeneExpression
 from .cluster_cells import ClusterCells
 from result import Result
+from tasks import Task
 from aws_xray_sdk.core import xray_recorder
 
 from config import get_config
@@ -17,25 +19,33 @@ config = get_config()
 
 
 class TaskFactory:
+    tasks = {t.__name__: t for t in (GetEmbedding,
+                                     ListGenes,
+                                     DifferentialExpression,
+                                     GeneExpression,
+                                     ClusterCells,
+                                     GetDoubletScore,
+                                     GetMitochondrialContent)
+             }
+
     def __init__(self):
         self.count_matrix = CountMatrix()
         self.count_matrix.sync()
 
-
     def submit(self, msg):
-        my_class = self._factory(msg)
+        task = self._factory(msg)
 
         # Try to perform task. If fails, send back an error to the API.
         try:
-            result = my_class.compute()
+            result = task.compute()
             return result
         except Exception as e:
             trace = traceback.format_exc()
             print(trace)
-            
+
             xray_recorder.current_segment().add_exception(e, trace)
 
-            # Do not send real traces in development.
+            # Only send real traces in development.
             if config.CLUSTER_ENV == "development":
                 result = [Result(json.dumps(trace), error=True)]
             else:
@@ -49,31 +59,12 @@ class TaskFactory:
                 ]
             return result
 
-    def _factory(self, msg):
+    def _factory(self, msg) -> Task:
         self.count_matrix.sync()
         task_def = msg.get("body", {})
         task_name = task_def.get("name")
 
-        if task_name == "GetEmbedding":
-            my_class = ComputeEmbedding(msg)
-            return my_class
-        elif task_name == "ListGenes":
-            my_class = ListGenes(msg)
-            return my_class
-        elif task_name == "DifferentialExpression":
-            my_class = DifferentialExpression(msg)
-            return my_class
-        elif task_name == "GeneExpression":
-            my_class = GeneExpression(msg)
-            return my_class
-        elif task_name == "ClusterCells":
-            my_class = ClusterCells(msg)
-            return my_class
-        elif task_name == "GetDoubletScore":
-            my_class = GetDoubletScore(msg)
-            return my_class
-        elif task_name == "GetMitochondrialContent":
-            my_class = GetMitochondrialContent(msg)
-            return my_class
-        else:
-            raise Exception("Task class with name {} was not found".format(task_name))
+        try:
+            return self.tasks[task_name]
+        except KeyError as e:
+            raise ValueError(f"Task class with name {task_name} was not found") from e
