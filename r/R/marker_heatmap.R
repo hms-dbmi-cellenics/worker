9ffc9862f59cfc51409bf1c507b14fac6f8915e9
ogibson-biomage <76957896+ogibson-biomage@users.noreply.github.com>
2021-07-20 17:21:10 -0300

    Update r/R/marker_heatmap.R
    Co-authored-by: Alex Pickering <alexvpickering@gmail.com>

--- r/R/marker_heatmap.R
+++ r/R/marker_heatmap.R
@@ -14,11 +14,9 @@ runMarkerHeatmap <- function(req, data) {
   all_markers <- presto::wilcoxauc(data, assay = "data", seurat_assay = "RNA")
   # Filtering out repeated genes to improve visualization, based on lowest p-value.
   # We could also use fold change.
-  pvalueAggregate <- aggregate(all_markers$padj, by = list(all_markers$feature), FUN = min)
-  minp <- pvalueAggregate$x[match(all_markers$feature, pvalueAggregate$Group.1)]
-  all_markers$minp <- minp
-  all_markers <- subset(all_markers, all_markers$padj <= all_markers$minp)
-  all_markers <- all_markers[!duplicated(all_markers$feature), ]
+ all_markers <- all_markers %>%  
+ group_by(feature) %>% 
+ slice(which.min(pval))
 
   nFeatures <- as.integer(30 / (as.integer(max(all_markers$group)) + 1))
   all_markers <- all_markers %>%
