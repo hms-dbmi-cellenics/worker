f48096cfe7f7585aa562b0c1c292a47ba00ecd40
Anugerah Erlaut <aerlaut@live.com>
2021-09-09 18:10:20 +0700

    create trajectory

--- r/R/trajectory_analysis.R
+++ r/R/trajectory_analysis.R
@@ -25,46 +25,46 @@ runTrajectoryAnalysis <- function(req, data){
         select(-cells_id)
 
     # Disabled until path algorithm can be confirmed
-    # ica_space_df <- t(cell_data@principal_graph_aux$UMAP$dp_mst) %>%
-    #   as.data.frame() %>%
-    #   dplyr::select(x = 1, y = 2) %>%
-    #   dplyr::mutate(cluster_name = rownames(.))
+    ica_space_df <- t(cell_data@principal_graph_aux$UMAP$dp_mst) %>%
+      as.data.frame() %>%
+      dplyr::select(x = 1, y = 2) %>%
+      dplyr::mutate(cluster_name = rownames(.))
 
-    # dp_mst <- cell_data@principal_graph$UMAP
+    dp_mst <- cell_data@principal_graph$UMAP
 
-    # edge_df <- dp_mst %>%
-    #   igraph::as_data_frame() %>%
-    #   dplyr::select(source = "from", target = "to") %>%
-    #   dplyr::left_join(ica_space_df %>%
-    #                      dplyr::select(source="cluster_name",
-    #                                     source_x="x",
-    #                                     source_y="y"),
-    #                    by = "source") %>%
-    #   dplyr::left_join(ica_space_df %>%
-    #                      dplyr::select(target="cluster_name",
-    #                                     target_x="x",
-    #                                     target_y="y"),
-    #                    by = "target")
+    edge_df <- dp_mst %>%
+      igraph::as_data_frame() %>%
+      dplyr::select(source = "from", target = "to") %>%
+      dplyr::left_join(ica_space_df %>%
+                         dplyr::select(source="cluster_name",
+                                        source_x="x",
+                                        source_y="y"),
+                       by = "source") %>%
+      dplyr::left_join(ica_space_df %>%
+                         dplyr::select(target="cluster_name",
+                                        target_x="x",
+                                        target_y="y"),
+                       by = "target")
 
-    # edges <- list()
-    # for (i in 1:nrow(edge_df)) {
-    #       edges <- append(edges,
-    #         list(
-    #             list(
-    #                 x1 = edge_df[i, "source_x"],
-    #                 y1 = edge_df[i, "source_y"],
-    #                 x2 = edge_df[i, "target_x"],
-    #                 y2 = edge_df[i, "target_y"]
-    #             )
-    #         )
-    #     )
-    # }
+    edges <- list()
+    for (i in 1:nrow(edge_df)) {
+          edges <- append(edges,
+            list(
+                list(
+                    x1 = edge_df[i, "source_x"],
+                    y1 = edge_df[i, "source_y"],
+                    x2 = edge_df[i, "target_x"],
+                    y2 = edge_df[i, "target_y"]
+                )
+            )
+        )
+    }
 
     return(
         list(
             pseudotime = unname(pseudotime),
-            # graph = edges -- disabled until path algorithm can be confirmed 
-            graph = list()
+            graph = edges #-- disabled until path algorithm can be confirmed 
+            # graph = list()
         )
     )
 }
