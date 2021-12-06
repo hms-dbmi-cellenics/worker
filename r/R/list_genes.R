ea38c20e202b71bc718749ace5c779d783d42f02
Alex Pickering <alexvpickering@gmail.com>
2021-12-06 13:42:41 -0800

    fix listGenes

--- r/R/list_genes.R
+++ r/R/list_genes.R
@@ -15,6 +15,7 @@
 #
 #' @export
 getList <- function(req, data) {
+  saveRDS(list(req = req, data=data), '/debug/getList.rds')
   select_fields <- req$body$selectFields
   order_by <- req$body$orderBy
   order_decreasing <- req$body$orderDirection == "DESC"
@@ -27,12 +28,13 @@ getList <- function(req, data) {
   colnames(gene_results)[colnames(gene_results) == "SYMBOL"] <- "gene_names"
   colnames(gene_results)[colnames(gene_results) == "variance.standardized"] <- "dispersions"
 
-  filter <- NULL
+  filters <- NULL
   if ("geneNamesFilter" %in% names(req$body)) {
-    filter <- req$body$geneNamesFilter
+    filters <- list(list(columnName = 'gene_names', expression = req$body$geneNamesFilter))
   }
 
-  paginated_results <- handle_pagination(gene_results, offset, limit, order_by, order_decreasing, filter)
+  filtered_results <- applyFilters(gene_results, filters)
+  paginated_results <- handlePagination(filtered_results, offset, limit, order_by, order_decreasing)
   gene_results <- paginated_results$gene_results
 
   columns <- c("gene_names", "dispersions")
