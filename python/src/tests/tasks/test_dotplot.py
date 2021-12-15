0ac9b07b6bd41236395dda5072b4caad99f03d15
Alex Pickering <alexvpickering@gmail.com>
2021-12-15 14:57:27 -0800

    init _format_request tests

--- python/src/tests/tasks/test_dotplot.py
+++ python/src/tests/tasks/test_dotplot.py
@@ -1,4 +1,7 @@
+import mock
 import pytest
+import responses
+from worker.helpers.mock_s3 import MockS3Class
 from worker.tasks.dotplot import DotPlot
 
 
@@ -8,21 +11,49 @@ class TestDotplot:
         self.correct_request = {
             "body": {
                 "name": "DotPlot",
-                "useMarkerGenes" : True,
+                "useMarkerGenes": True,
                 "numberOfMarkers": 3,
                 "customGenesList": ["Gene1", "Gene2", "Gene3"],
-                "groupBy": "louvain",
-                "filterBy": {
-                    "group": "All",
-                    "key": "All"
-                }
+                "groupBy": "cluster1",
+                "filterBy": {"group": "All", "key": "All"},
             }
         }
+
+    """
+    Mocks the S3 query for fetching cell sets. Returns an
+    empty cell set and yields the patched up object.
+    """
+
+    @pytest.fixture
+    def mock_S3_get(self):
+        with mock.patch("boto3.client") as m:
+            mockS3 = MockS3Class()
+            m.return_value = mockS3
+            yield (m, mockS3)
 
     def test_throws_on_missing_parameters(self):
         with pytest.raises(TypeError):
             DotPlot()
 
-    def test_works_with_request(self):
-        DotPlot(self.correct_request).compute()
-        assert True
+    @responses.activate
+    def test_generates_correct_request(self, mock_S3_get):
+        MockS3Class.setResponse("one_set")
+        request = DotPlot(self.correct_request)._format_request()
+        assert isinstance(request, dict)
+
+        # all expected keys are in the request
+        expected_keys = [
+            "useMarkerGenes",
+            "numberOfMarkers",
+            "customGenesList",
+            "groupBy",
+            "filterBy",
+            "applyFilter",
+        ]
+        assert all(key in request for key in expected_keys)
+
+    @responses.activate
+    def test_group_by_equals_filter_by_when_filter_by_is_all(self, mock_S3_get):
+        MockS3Class.setResponse("one_set")
+        request = DotPlot(self.correct_request)._format_request()
+        assert request["filterBy"] == request["groupBy"]
