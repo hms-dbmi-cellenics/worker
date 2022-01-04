88ee07434a95ca3f332066ea660bb649b242f981
german <gerbeldo@gmail.com>
2022-01-04 12:18:25 -0300

    add test-cluster.R skeleton and mock_req

---
+++ r/tests/testthat/test-cluster.R
@@ -0,0 +1,23 @@
+mock_req <- function() {
+  req <- list(
+    body =
+      list(
+        config = list(
+          resolution = 0.5
+        ),
+        type = "louvain"
+      )
+  )
+}
+
+mock_scdata <- function() {
+  asd
+}
+
+test_that("louvain clustering works", {
+
+})
+
+test_that("leiden clustering works", {
+
+})
