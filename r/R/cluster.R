21529bcbbc586a52d19cc36b923b17ce732ac25e
german <gerbeldo@gmail.com>
2022-01-04 12:22:14 -0300

    format cluster.R

--- r/R/cluster.R
+++ r/R/cluster.R
@@ -1,6 +1,7 @@
 #
 # getClusters
-# returns the clusters in the shape of a dataframe with a clusters column, cell ids column and cell barcode as rownames.
+# returns the clusters in the shape of a dataframe with a clusters column,
+# cell ids column and cell barcode as rownames.
 #
 # req$body has:
 # type: can be "louvain"/"leiden"
@@ -9,7 +10,8 @@
 #         }
 #
 #
-# We currently CANT support leiden, we need to discuss this in bioinformatics, the algorithm is not working.
+# We currently CANT support leiden, we need to discuss this in bioinformatics,
+#  the algorithm is not working.
 #
 #' @export
 #'
@@ -18,14 +20,15 @@ runClusters <- function(req, data) {
   type <- req$body$type
 
   data <- getClusters(type, resol, data)
-  res_col <- paste0(data@active.assay, "_snn_res.",toString(resol))
-  # In the meta data slot the clustering is stored with the resolution used to calculate it
+  res_col <- paste0(data@active.assay, "_snn_res.", toString(resol))
+  # In the meta data slot the clustering is stored with the resolution
+  # used to calculate it
   # RNA_snn_res.#resolution
-  df <- data.frame(cluster = data@meta.data[, res_col], cell_ids = data@meta.data$cells_id)
+  df <- data.frame(
+    cluster = data@meta.data[, res_col],
+    cell_ids = data@meta.data$cells_id
+  )
   # get the cell barcodes as rownames
   rownames(df) <- rownames(data@meta.data)
   return(df)
 }
-
-
-
