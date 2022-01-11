86f2d8eae2c5f68c5c9ab88c966d768842fee199
Alex Pickering <alexvpickering@gmail.com>
2022-01-11 10:25:22 -0800

    move getSNNiGraph to cluster.R

--- r/R/cluster.R
+++ r/R/cluster.R
@@ -76,3 +76,30 @@ getClusters <- function(type, resolution, data) {
   return(data)
 }
 
+
+#' Get and Convert SNN Graph object into igraph object
+#'
+#' This is used to facilitate leiden clustering.
+#'
+#' @param data \code{Seurat} object
+#'
+#' @return boolean indicating if SNN Graph object exists
+#'
+getSNNiGraph <- function(data) {
+
+  # check to see if we already have Seurat SNN Graph object
+  snn_name <- paste0(data@active.assay, "_snn")
+
+  # if doesn't exist, run SNN
+  if (!snn_name %in% names(data)) data <- Seurat::FindNeighbors(data)
+
+  # convert Seurat Graph object to igraph
+  # similar to https://github.com/joshpeters/westerlund/blob/46609a68855d64ed06f436a6e2628578248d3237/R/functions.R#L85
+  adj_matrix <- Matrix::Matrix(as.matrix(data@graphs[[snn_name]]), sparse = TRUE)
+  g <- igraph::graph_from_adjacency_matrix(adj_matrix,
+                                           mode = "undirected",
+                                           weighted = TRUE
+  )
+  return(g)
+}
+
