987879a12bb8f25a3c311821582b52a4ec0d50e0
Oliver Gibson <olivergibson@MacBook-Pro-de-Oliver.local>
2022-02-01 11:27:16 -0300

    merge master

--- r/R/cluster.R
+++ r/R/cluster.R
@@ -34,7 +34,6 @@ runClusters <- function(req, data) {
     )
     # get the cell barcodes as rownames
     rownames(df) <- rownames(data@meta.data)
-
   }
 
   return(df)
@@ -52,6 +51,7 @@ runClusters <- function(req, data) {
 getClusters <- function(type, resolution, data) {
   res_col <- paste0(data@active.assay, "_snn_res.", toString(resolution))
   algorithm <- list("louvain" = 1, "leiden" = 4)[[type]]
+<<<<<<< HEAD
 
   # use the reduction from data integration for nearest neighbors graph
   if ("active.reduction" %in% names(data@misc)) {
@@ -93,6 +93,49 @@ getClusters <- function(type, resolution, data) {
 #'
 getSNNiGraph <- function(data, active.reduction) {
 
+=======
+
+  # use the reduction from data integration for nearest neighbors graph
+  if ("active.reduction" %in% names(data@misc)) {
+    active.reduction <- data@misc[["active.reduction"]]
+  } else {
+    active.reduction <- "pca"
+  }
+
+  if (type == "leiden") {
+
+    # emulate FindClusters, which overwrites seurat_clusters slot and meta.data column
+    g <- getSNNiGraph(data, active.reduction)
+    clus_res <- igraph::cluster_leiden(g, "modularity", resolution_parameter = resolution)
+    clusters <- clus_res$membership
+    names(clusters) <- clus_res$names
+    clusters <- clusters[colnames(data)]
+    data$seurat_clusters <- data@meta.data[, res_col] <- factor(clusters - 1)
+
+  } else {
+
+    graph.name <- paste0(Seurat::DefaultAssay(data), "_snn")
+    if (!graph.name %in% names(data)) {
+      data <- Seurat::FindNeighbors(data, annoy.metric = "cosine", verbose = FALSE, reduction = active.reduction)
+    }
+    data <- Seurat::FindClusters(data, resolution = resolution, verbose = FALSE, algorithm = algorithm)
+  }
+
+  return(data)
+}
+
+
+#' Get and Convert SNN Graph object into igraph object
+#'
+#' This is used to facilitate leiden clustering.
+#'
+#' @param data \code{Seurat} object
+#'
+#' @return boolean indicating if SNN Graph object exists
+#'
+getSNNiGraph <- function(data, active.reduction) {
+
+>>>>>>> 467e807 (merge master)
   # check to see if we already have Seurat SNN Graph object
   snn_name <- paste0(data@active.assay, "_snn")
 
