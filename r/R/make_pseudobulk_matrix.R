90913fe49b85c1c952ec7c038e2d5f95faae359b
german <gerbeldo@gmail.com>
2022-02-02 09:52:47 -0300

    careful level spec and remove group key function
    Co-authored-by: Alex Pickering <alexvpickering@gmail.com>

--- r/R/make_pseudobulk_matrix.R
+++ r/R/make_pseudobulk_matrix.R
@@ -17,46 +17,25 @@ makePseudobulkMatrix <- function(scdata) {
   gene_annotations <- scdata@misc$gene_annotations
 
   groups <- factor(scdata$samples)
-  group_key <- getSampleGroupKey(scdata)
+  # aggregate over samples
+  samples <- scdata$samples
+  samples <- factor(samples, levels = unique(samples))
 
-  agg <- presto::sumGroups(counts, groups, MARGIN = 1)
+  agg <- presto::sumGroups(counts, samples, MARGIN = 1)
   agg <- Matrix::Matrix(agg, sparse = TRUE)
   agg <- Matrix::t(agg)
 
   #row/colnames are lost in aggregation
   rownames(agg) <- rownames(counts)
-  colnames(agg) <- levels(groups)
+  colnames(agg) <- levels(samples)
 
   # recover original metadata
-  metadata <- data.frame(samples = colnames(agg))
-  # makes sure that original custom value goes to correct sample
-  metadata <- dplyr::inner_join(metadata, group_key, by = "samples")
-  rownames(metadata) <- metadata$samples
+  metadata <- data.frame(samples = colnames(agg),
+                         custom = scdata$custom[!duplicated(samples)],
+                         row.names = colnames(agg))
 
   # create seurat, and add metadata
-  pbulk <- Seurat::CreateSeuratObject(agg)
-  pbulk <- Seurat::AddMetaData(pbulk, metadata)
+  pbulk <- Seurat::CreateSeuratObject(agg, meta.data = metadata)
   pbulk@misc$gene_annotations <- gene_annotations
 
   return(pbulk)
-}
-
-
-
-#' create sample group key table
-#'
-#' Makes a table with the original sample and custom values, allowing
-#' to not have to trust in the groupings/sample orders.
-#'
-#' @param scdata A filtered SeuratObject
-#'
-#' @return a data.frame with a row per sample/custom
-#' @export
-#'
-#' @examples
-getSampleGroupKey <- function(scdata) {
-  metadata <- unique(dplyr::select(scdata@meta.data, samples, custom))
-  rownames(metadata) <- NULL
-
-  return(metadata)
-}
