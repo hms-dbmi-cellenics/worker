011b32b2c0cc2c4c1b1c6178d4bbc5eb31a0fe91
german <gerbeldo@gmail.com>
2022-02-11 13:03:26 -0300

    styler

--- r/R/pseudobulk_differential_expression.R
+++ r/R/pseudobulk_differential_expression.R
@@ -6,53 +6,54 @@
 #' @export
 #'
 runPseudobulkDE <- function(pbulk) {
-    group <- pbulk$custom
+  group <- pbulk$custom
 
-    y <- edgeR::DGEList(
-        pbulk[['RNA']]@counts,
-        samples = data.frame(group),
-        genes = pbulk@misc$gene_annotations[, "name", drop = FALSE])
+  y <- edgeR::DGEList(
+    pbulk[["RNA"]]@counts,
+    samples = data.frame(group),
+    genes = pbulk@misc$gene_annotations[, "name", drop = FALSE]
+  )
 
 
-    keep <- edgeR::filterByExpr(y, group=group)
+  keep <- edgeR::filterByExpr(y, group = group)
 
-    # get filtered genes to add to results
-    disc <- row.names(y)[!keep]
+  # get filtered genes to add to results
+  disc <- row.names(y)[!keep]
 
-    y <- y[keep,]
-    y <- edgeR::calcNormFactors(y)
+  y <- y[keep, ]
+  y <- edgeR::calcNormFactors(y)
 
-    design <- stats::model.matrix(~0 + group)
-    colnames(design) <- gsub('^group', '', colnames(design))
+  design <- stats::model.matrix(~ 0 + group)
+  colnames(design) <- gsub("^group", "", colnames(design))
 
-    # silences warning when no replication (1 vs 1 sample comparisons)
-    v <- suppressWarnings(limma::voomWithQualityWeights(y, design))
-    fit <- limma::lmFit(v)
+  # silences warning when no replication (1 vs 1 sample comparisons)
+  v <- suppressWarnings(limma::voomWithQualityWeights(y, design))
+  fit <- limma::lmFit(v)
 
-    # make sure we get the right contrast
-    contrast <- 'base-background'
-    contrast_matrix <- limma::makeContrasts(contrasts = contrast, levels = design)
-    fit <- limma::contrasts.fit(fit, contrast_matrix)
+  # make sure we get the right contrast
+  contrast <- "base-background"
+  contrast_matrix <- limma::makeContrasts(contrasts = contrast, levels = design)
+  fit <- limma::contrasts.fit(fit, contrast_matrix)
 
-    # calculate logFC or run DE based on sample size
-    nsample <- ncol(y)
-    if (nsample  == 2) {
-        # only logFC if two samples
-        res <- data.frame(fit$genes,
-                          logFC = fit$coefficients[, contrast],
-                          AveExpr = fit$Amean)
+  # calculate logFC or run DE based on sample size
+  nsample <- ncol(y)
+  if (nsample == 2) {
+    # only logFC if two samples
+    res <- data.frame(fit$genes,
+      logFC = fit$coefficients[, contrast],
+      AveExpr = fit$Amean
+    )
 
-        res <- res[order(abs(res$logFC), decreasing = TRUE), ]
+    res <- res[order(abs(res$logFC), decreasing = TRUE), ]
+  } else {
+    # differential expression if 3+ samples
+    eb_fit <- limma::eBayes(fit, robust = TRUE)
+    res <- limma::topTable(eb_fit, coef = contrast, sort.by = "p", n = Inf)
+  }
 
-    } else {
-        # differential expression if 3+ samples
-        eb_fit <- limma::eBayes(fit, robust=TRUE)
-        res <- limma::topTable(eb_fit, coef = contrast, sort.by="p", n=Inf)
-    }
+  # add filtered genes so that searchable
+  res[disc, ] <- NA
+  res[disc, "name"] <- pbulk@misc$gene_annotations[disc, "name"]
 
-    # add filtered genes so that searchable
-    res[disc, ] <- NA
-    res[disc, 'name'] <- pbulk@misc$gene_annotations[disc, 'name']
-
-    return(res)
+  return(res)
 }
