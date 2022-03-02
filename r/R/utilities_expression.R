98fe8d9dbb3e85d0d391d84205aa7ed44d07dabd
Oliver Gibson <olivergibson@MacBook-Pro-de-Oliver.local>
2022-03-02 16:09:41 -0300

    separate worker_helpers

---
+++ r/R/utilities_expression.R
@@ -0,0 +1,62 @@
+#' Returns expression values for selected genes
+#'
+#' @param genes - Must have names and input(ensmbl ids)
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getExpressionValues <- function(genes, data) {
+  quantile_threshold <- 0.95
+  #
+  # Get the expression values for those genes in the corresponding matrix.
+  geneExpression <- data@assays$RNA@data[unique(genes$input), , drop = FALSE]
+  geneExpression <- as.data.frame(t(as.matrix(geneExpression)))
+  geneExpression$cells_id <- data@meta.data$cells_id
+  geneExpression <- geneExpression[order(geneExpression$cells_id), ]
+  geneExpression <- geneExpression %>%
+    tidyr::complete(cells_id = seq(0, max(data@meta.data$cells_id))) %>%
+    dplyr::select(-cells_id) %>%
+    as.data.frame()
+  # worried about duplicate gene row.names in @data
+  symbol_idx <- match(colnames(geneExpression), genes$input)
+  colnames(geneExpression) <- genes$name[symbol_idx]
+  adjGeneExpression <- as.data.frame(apply(geneExpression, 2, FUN = function(x) {
+    lim <- as.numeric(quantile(x, quantile_threshold, na.rm = TRUE))
+    i <- 0.01
+    while (lim == 0 & i + quantile_threshold <= 1) {
+      lim <- as.numeric(quantile(x, quantile_threshold + i, na.rm = TRUE))
+      i <- i + 0.01
+    }
+    return(pmin(x, lim))
+  }))
+  return(list(rawExpression = geneExpression, truncatedExpression = adjGeneExpression))
+}
+
+#' formatExpression
+#'
+#' @param data
+#'
+#' @return Formats expression values for the UI.
+#' @export
+#'
+#' @examples
+formatExpression <- function(data) {
+  return(purrr::map2(data$rawExpression, data$truncatedExpression, formatExpressionAux))
+}
+
+formatExpressionAux <- function(raw, trunc) {
+  return(list(
+    rawExpression = list(
+      mean = mean(raw),
+      stdev = sd(raw),
+      expression = raw
+    ),
+    truncatedExpression = list(
+      min = min(trunc),
+      max = max(trunc),
+      expression = trunc
+    )
+  ))
+}
---
+++ r/R/utilities_markers.R
@@ -0,0 +1,51 @@
+#' getTopMarkerGenes
+#'
+#' @param nFeatures
+#' @param data
+#' @param cellSets
+#' @param aucMin
+#' @param pctInMin
+#' @param pctOutMax
+#'
+#' @return Returns top marker genes as calculated by presto
+#' @export
+#'
+#' @examples
+getTopMarkerGenes <- function(nFeatures, data, cellSets, aucMin = 0.3, pctInMin = 20, pctOutMax = 70) {
+  data$marker_groups <- NA
+
+  object_ids <- data$cells_id
+  for (i in seq_along(cellSets)) {
+    set <- cellSets[[i]]
+    filtered_cells <- intersect(set$cellIds, object_ids)
+    data$marker_groups[object_ids %in% filtered_cells] <- i
+  }
+
+  all_markers <- presto::wilcoxauc(data, group_by = "marker_groups", assay = "data", seurat_assay = "RNA")
+  all_markers$group <- as.numeric(all_markers$group)
+
+  # may not return nFeatures markers per cluster if values are too stringent
+  filtered_markers <- all_markers %>%
+    dplyr::filter(logFC > 0 &
+                    auc >= aucMin &
+                    pct_in >= pctInMin &
+                    pct_out <= pctOutMax) %>%
+    dplyr::group_by(feature) %>%
+    dplyr::slice(which.min(pval))
+
+  top_markers <- filtered_markers %>%
+    dplyr::group_by(group) %>%
+    dplyr::arrange(dplyr::desc(logFC)) %>%
+    dplyr::slice_head(n = nFeatures) %>%
+    dplyr::arrange(group)
+
+  message(sprintf("%d markers selected", nrow(top_markers)))
+  return(top_markers)
+}
+
+getMarkerNames <- function(data, all_markers) {
+  all_markers$name <- data@misc$gene_annotations[all_markers$feature, "name"]
+  all_markers <- all_markers %>% dplyr::transmute(input = feature, name = name)
+  rownames(all_markers) <- c()
+  return(all_markers)
+}
