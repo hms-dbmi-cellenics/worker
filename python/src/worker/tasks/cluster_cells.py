2aed196676d9305af54743dd6c87fd46a1610170
german <gerbeldo@gmail.com>
2022-03-31 18:34:02 -0400

    comment python funs moved to R

--- python/src/worker/tasks/cluster_cells.py
+++ python/src/worker/tasks/cluster_cells.py
@@ -1,17 +1,18 @@
 import json
-from logging import info
 
 import backoff
-import pandas as pd
+# import pandas as pd
 import requests
 from aws_xray_sdk.core import xray_recorder
 from exceptions import raise_if_error
-from natsort import natsorted
 
 from ..config import config
 from ..helpers.color_pool import COLOR_POOL
 from ..result import Result
 from ..tasks import Task
+
+# from logging import info
+# from natsort import natsorted
 
 
 class ClusterCells(Task):
@@ -20,55 +21,55 @@ class ClusterCells(Task):
         self.colors = COLOR_POOL.copy()
         self.request = msg
 
-    def _convert_to_cell_set_object(self, raw):
-        # construct new cell set group
-        cell_set_key = self.task_def["cellSetKey"]
-        cell_set_name = self.task_def["cellSetName"]
+    # def _convert_to_cell_set_object(self, raw):
+    #     # construct new cell set group
+    #     cell_set_key = self.task_def["cellSetKey"]
+    #     cell_set_name = self.task_def["cellSetName"]
 
-        cell_set = {
-            "key": cell_set_key,
-            "name": cell_set_name,
-            "rootNode": True,
-            "type": "cellSets",
-            "children": [],
-        }
+    #     cell_set = {
+    #         "key": cell_set_key,
+    #         "name": cell_set_name,
+    #         "rootNode": True,
+    #         "type": "cellSets",
+    #         "children": [],
+    #     }
 
-        for cluster in natsorted(raw["cluster"].cat.categories):
-            view = raw[raw.cluster == cluster]["cell_ids"]
-            cell_set["children"].append(
-                {
-                    "key": f"{cell_set_key}-{cluster}",
-                    "name": f"Cluster {cluster}",
-                    "rootNode": False,
-                    "type": "cellSets",
-                    "color": self.colors.pop(0),
-                    "cellIds": list(view.map(int)),
-                }
-            )
+    #     for cluster in natsorted(raw["cluster"].cat.categories):
+    #         view = raw[raw.cluster == cluster]["cell_ids"]
+    #         cell_set["children"].append(
+    #             {
+    #                 "key": f"{cell_set_key}-{cluster}",
+    #                 "name": f"Cluster {cluster}",
+    #                 "rootNode": False,
+    #                 "type": "cellSets",
+    #                 "color": self.colors.pop(0),
+    #                 "cellIds": list(view.map(int)),
+    #             }
+    #         )
 
-        return cell_set
+    #     return cell_set
 
-    def _update_through_api(self, cell_set_object):
-        r = requests.patch(
-            f"{config.API_URL}/v1/experiments/{self.request['experimentId']}/cellSets",
-            headers={
-                "content-type": "application/boschni-json-merger+json",
-                "Authorization": self.request["Authorization"],
-            },
-            json=[
-                {
-                    "$match": {
-                        "query": f'$[?(@.key == "{self.task_def["cellSetKey"]}")]',
-                        "$remove": True,
-                    },
-                },
-                {
-                    "$prepend": cell_set_object,
-                },
-            ],
-        )
+    # def _update_through_api(self, cell_set_object):
+    #     r = requests.patch(
+    #         f"{config.API_URL}/v1/experiments/{self.request['experimentId']}/cellSets",
+    #         headers={
+    #             "content-type": "application/boschni-json-merger+json",
+    #             "Authorization": self.request["Authorization"],
+    #         },
+    #         json=[
+    #             {
+    #                 "$match": {
+    #                     "query": f'$[?(@.key == "{self.task_def["cellSetKey"]}")]',
+    #                     "$remove": True,
+    #                 },
+    #             },
+    #             {
+    #                 "$prepend": cell_set_object,
+    #             },
+    #         ],
+    #     )
 
-        info(r.status_code)
+    #     info(r.status_code)
 
     def _format_result(self, result):
         return Result(result, cacheable=False)
@@ -106,12 +107,12 @@ class ClusterCells(Task):
         # This is a questionable bit of code, but basically it was a simple way of adjusting the results to the shape
         # expected by the UI Doing this allowed me to use the format function as is. It shouldn't be too taxing,
         # at most O(n of cells), which is well within our time complexity because the taxing part will be clustering.
-        df = pd.DataFrame(data)
-        df.set_index("_row", inplace=True)
-        df["cluster"] = pd.Categorical(df.cluster)
+        # df = pd.DataFrame(data)
+        # df.set_index("_row", inplace=True)
+        # df["cluster"] = pd.Categorical(df.cluster)
 
         # Convert it into a JSON format and patch the API directly
-        cell_set_object = self._convert_to_cell_set_object(df)
-        self._update_through_api(cell_set_object)
+        # cell_set_object = self._convert_to_cell_set_object(df)
+        # self._update_through_api(cell_set_object)
 
-        return self._format_result(cell_set_object)
+        return self._format_result(data)
