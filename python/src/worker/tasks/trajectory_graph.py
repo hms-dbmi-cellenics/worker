4477d525379537a43cef1695be919bec4fb457a2
sara castellano <sara@biomage.net>
2022-06-28 08:41:31 +0200

    split files for tasks

--- python/src/worker/tasks/trajectory_graph.py
+++ python/src/worker/tasks/trajectory_graph.py
@@ -9,36 +9,6 @@ from ..result import Result
 from ..tasks import Task
 
 
-class GetPseudoTime(Task):
-    def _format_result(self, raw):
-        # JSONify result.
-        raw_result = json.dumps(raw)
-
-        # Return a list of formatted results.
-        return [Result(raw_result), Result(raw_result)]
-
-    @xray_recorder.capture("GetPseudoTime.compute")
-    @backoff.on_exception(
-        backoff.expo, requests.exceptions.RequestException, max_time=30
-    )
-    def compute(self):
-        request = {
-            "rootNodes": self.task_def["rootNodes"],
-        }
-
-        r = requests.post(
-            f"{config.R_WORKER_URL}/v0/runTrajectoryAnalysis",
-            headers={"content-type": "application/json"},
-            data=json.dumps(request),
-        )
-
-        # raise an exception if an HTTPError if one occurred because otherwise r.json() will fail
-        r.raise_for_status()
-        # The index order relies on cells_id in an ascending form. The order is made in the R part.
-        result = r.json()
-        return self._format_result(result)
-
-
 class GetTrajectoryGraph(Task):
     def _format_result(self, raw):
         # JSONify result.
