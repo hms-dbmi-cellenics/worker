9f7c77567e65a74cb7b475c0500f32de902abeaa
Sara Castellano <37797231+saracastel@users.noreply.github.com>
2022-07-01 16:14:29 +0200

    update documentation
    Co-authored-by: german <gerbeldo@gmail.com>

--- r/R/trajectory_analysis.R
+++ r/R/trajectory_analysis.R
@@ -108,7 +108,7 @@ generateGraphData <- function(data) {
 #' For this reason, there is a need to add to the dataframe the corresponding cell ids for cell barcodes that were filtered out and fill the values with NULL.
 #'
 #' @param df data.frame with barcodes as rownames
-#' @param data Seurat object with cells id stored in: data@@meta.data$cells_id
+#' @param data Seurat object with cells_id
 #'
 #' @return a tibble filled with NULL values for missing cell ids
 #' @export
