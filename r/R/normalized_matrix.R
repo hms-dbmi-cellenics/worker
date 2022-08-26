55ec3445390ada8c59168175c9bcfdc60656f6b7
sara castellano <sara@biomage.net>
2022-08-26 12:13:35 +0200

    add R worker function

---
+++ r/R/normalized_matrix.R
@@ -0,0 +1,37 @@
+#' Extract normalized expression matrix
+#'
+#' This function subsets the Seurat object using the cell IDs received from the
+#' python worker, then extracts and returns the normalized expression matrix.
+#'
+#' Data can be subsetted in the python worker according to one of clusters, samples,
+#' and metadata groups, or a combination of them.
+#'
+#' @param req {body: {
+#'               filterBy: Cellsets to subset the experiment with
+#'               applyFilter: TRUE/FALSE determines whether to subset the data
+#'              }
+#'            }
+#' @param data SeuratObject
+#'
+#' @return
+#' @export
+#'
+GetNormalizedExpression <- function(req, data) {
+  apply_filter <- req$body$applyFilter
+  filter_by <- req$body$filterBy
+  subset_ids <- unlist(filter_by$cellIds)
+
+  if (apply_filter == TRUE) {
+    message("Number of cells before subsetting: ", ncol(data))
+    data <- subsetIds(data, cells_id = subset_ids)
+
+    message("Extracting normalized expression matrix from subsetted data")
+    norm_matrix <- as.data.frame(Seurat::GetAssayData(data, slot = "data", assay = "RNA"))
+    message("Number of cells after subsetting: ", ncol(norm_matrix))
+  } else {
+    message("Extracting normalized expression matrix from whole data")
+    norm_matrix <- as.data.frame(Seurat::GetAssayData(data, slot = "data", assay = "RNA"))
+  }
+
+  return(norm_matrix)
+}
