a53e5dd3a83aa7e630e78ae699fde42add9339d8
German Beldorati Stark <gerbeldo@gmail.com>
2022-09-24 12:19:26 -0300

    fix expression tests

--- r/tests/testthat/test-expression.R
+++ r/tests/testthat/test-expression.R
@@ -1,5 +1,6 @@
 mock_req <- function() {
   req <- list(body = list(genes = list("MS4A1", "CD79B")))
+  return(req)
 }
 
 mock_scdata <- function() {
@@ -19,37 +20,112 @@ test_that("Expression task returns appropriate number and names of genes.", {
 
   res <- runExpression(req, data)
 
-  expect_equal(length(res), 2)
-  expect_equal(as.list(names(res)), req$body$genes)
+  expect_equal(
+    names(res),
+    c("order", "stats", "rawExpression", "truncatedExpression", "zScore")
+  )
+  expect_equal(length(res$order), 2)
+  expect_equal(length(res$stats), 2)
+  expect_equal(res$order, req$body$genes)
+  expect_equal(names(res$stats), unlist(req$body$genes))
 })
+
+test_that("Expression matrices are correctly formatted for mathJS", {
+  data <- mock_scdata()
+  req <- mock_req()
+
+  res <- runExpression(req, data)
+
+  exp <- list(
+    raw = res$rawExpression,
+    trunc = res$truncatedExpression,
+    z = res$zScore
+  )
+  # expression matrices contain 4 attributes
+  expect_equal(unlist(unique(lapply(exp, length))), 4)
+
+  # expression matrices contain correctly named attributes
+  expect_equal(
+    unlist(unique(lapply(exp, names))),
+    c("values", "index", "ptr", "size")
+  )
+
+  # no NA values
+  expect_true(!all(unlist(lapply(res$rawExpression, is.na))))
+  expect_true(!all(unlist(lapply(res$truncatedExpression, is.na))))
+  expect_true(!all(unlist(lapply(res$zScore, is.na))))
+})
+
 
 test_that("Expression task returns appropriate number of cells.", {
   data <- mock_scdata()
   req <- mock_req()
 
   res <- runExpression(req, data)
-  raw_expression <- res[[1]]$rawExpression$expression
-  truncated_expression <- res[[1]]$truncatedExpression$expression
 
+  exp <- list(
+    raw = res$rawExpression,
+    trunc = res$truncatedExpression,
+    z = res$zScore
+  )
 
-  expect_equal(length(raw_expression), length(truncated_expression))
-
-  expect_equal(length(raw_expression), ncol(data))
+  expect_equal(unique(unlist(lapply(exp, function(x) x$size[[1]]))), max(data$cells_id) + 1)
 })
 
-test_that("Expression result is properly formatted.", {
+test_that("summaryStats calculates correct summary stats.", {
   data <- mock_scdata()
   req <- mock_req()
+  gene_annotations <- data@misc$gene_annotations
 
-  res <- runExpression(req, data)
-  raw_expression_result <- res[[1]]$rawExpression
-  truncated_expression_result <- res[[1]]$truncatedExpression
-  expect_true(!all(is.na(raw_expression_result$expression)))
+  gene_subset <-
+    subset(
+      gene_annotations,
+      toupper(gene_annotations$name) %in% toupper(req$body$genes)
+    )
 
-  expect_equal(raw_expression_result$mean, mean(raw_expression_result$expression, na.rm = TRUE))
-  expect_equal(raw_expression_result$stdev, sd(raw_expression_result$expression, na.rm = TRUE))
-  expect_equal(truncated_expression_result$min, min(truncated_expression_result$expression, na.rm = TRUE))
-  expect_equal(truncated_expression_result$max, max(truncated_expression_result$expression, na.rm = TRUE))
+
+  expression_values <- getExpressionValues(data, genesSubset)
+  res <- summaryStats(expression_values)
+
+  expect_equal(names(res), unlist(req$body$genes))
+  expect_equal(
+    unique(unlist(lapply(res, names))),
+    c("rawMean", "rawStdev", "truncatedMin", "truncatedMax")
+  )
+
+  expect_equal(
+    res$MS4A1$rawMean,
+    mean(expression_values$rawExpression$MS4A1, na.rm = TRUE)
+  )
+  expect_equal(
+    res$MS4A1$rawStdev,
+    sd(expression_values$rawExpression$MS4A1, na.rm = TRUE)
+  )
+  expect_equal(
+    res$MS4A1$truncatedMin,
+    min(expression_values$truncatedExpression$MS4A1)
+  )
+  expect_equal(
+    res$MS4A1$truncatedMax,
+    max(expression_values$truncatedExpression$MS4A1)
+  )
+
+  expect_equal(
+    res$CD79B$rawMean,
+    mean(expression_values$rawExpression$CD79B, na.rm = TRUE)
+  )
+  expect_equal(
+    res$CD79B$rawStdev,
+    sd(expression_values$rawExpression$CD79B, na.rm = TRUE)
+  )
+  expect_equal(
+    res$CD79B$truncatedMin,
+    min(expression_values$truncatedExpression$CD79B)
+  )
+  expect_equal(
+    res$CD79B$truncatedMax,
+    max(expression_values$truncatedExpression$CD79B)
+  )
 })
 
 
@@ -60,7 +136,17 @@ test_that("Expression task continues if gene doesn't exist and returns appropria
   req$body$genes <- append(req$body$genes, "aaa")
 
   res <- runExpression(req, data)
-  expect_equal(length(res), 2)
+
+  exp <- list(
+    raw = res$rawExpression,
+    trunc = res$truncatedExpression,
+    z = res$zScore
+  )
+
+  expect_equal(length(res$order), 2)
+  expect_equal(length(res$stats), 2)
+
+  expect_equal(unique(unlist(lapply(exp, function(x) x$size[[2]]))), 2)
 })
 
 test_that("Expression task works with one gene", {
@@ -69,19 +155,28 @@ test_that("Expression task works with one gene", {
   req$body$genes <- list("MS4A1")
 
   res <- runExpression(req, data)
-  expect_equal(length(res), 1)
-  expect_equal(length(res[[1]]$rawExpression$expression), ncol(data))
+
+  exp <- list(
+    raw = res$rawExpression,
+    trunc = res$truncatedExpression,
+    z = res$zScore
+  )
+
+  expect_equal(length(res$order), 1)
+  expect_equal(length(res$stats), 1)
+
+  expect_equal(unique(unlist(lapply(exp, function(x) x$size[[2]]))), 1)
 })
 
 test_that("If the max truncated expression value is 0, iterates correctly to find a non zero value", {
   data <- mock_scdata()
   req <- mock_req()
 
-  data@assays$RNA@data["MS4A1",] <- 0
-  data@assays$RNA@data["MS4A1",1] <- 5
+  data@assays$RNA@data["MS4A1", ] <- 0
+  data@assays$RNA@data["MS4A1", 1] <- 5
 
   res <- runExpression(req, data)
-  expect_false(all(res$MS4A1$truncatedExpression$expression==0) )
+  expect_false(all(res$truncatedExpression$values == 0))
 })
 
 test_that("runExpression throws an error if request only non existing genes", {
