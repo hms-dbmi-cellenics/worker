a732440f629faf4f46b537bdb51f311797cf3232
cosa65 <martin@biomage.net>
2022-10-10 18:22:35 -0300

    WIP

--- python/src/worker/tasks/normalized_matrix.py
+++ python/src/worker/tasks/normalized_matrix.py
@@ -11,6 +11,17 @@ from ..helpers.s3 import get_cell_sets
 from ..result import Result
 from ..tasks import Task
 
+# If keys_to_subset
+def get_cell_ids(category, cell_sets, keys_to_subset = []):
+    # Get all cell sets in the category
+    cell_sets_in_category = next(cell_class for cell_class in cell_sets if cell_class["key"] == category)["children"]
+
+    cell_ids = set()
+
+    for cell_set in cell_sets_in_category:
+        if (keys_to_subset == True or cell_set["key"] in keys_to_subset):
+            cell_ids = cell_ids.union(set(cell_set["cellIds"]))
+
 
 class GetNormalizedExpression(Task):
     def __init__(self, msg):
@@ -24,32 +35,60 @@ class GetNormalizedExpression(Task):
         return Result(result)
 
     def _format_request(self):
-        filterBy = self.task_def["filterBy"]
-        applyFilter = all(group.lower() != "all" for group in filterBy["group"])
+        subset_by = self.task_def["subsetBy"]
+        # apply_filter = all(group.lower() != "all" for group in subset_by["group"])
 
-        if not applyFilter:
-            return {
-            "filterBy": [],
-            "applyFilter": applyFilter,
-        }
+        # sample: [],
+        # louvain: [],
+        # metadata: [],
+        # scratchpad: [],
 
-        # Getting cell ids to subset the seurat object with a group of cells
-        cellSets = get_cell_sets(self.experiment_id) 
-        filterByCellSets = []
-        for cellSet in cellSets:
-                if cellSet["key"] in filterBy["group"]:
-                    filterByCellSets.append(cellSet)
+        cell_sets = get_cell_sets(self.experiment_id)
 
-        filterByCellIds = []
-        for cellSet in filterByCellSets:
-            for child in cellSet["children"]:
-                if child["key"] in filterBy["key"]:
-                    filterByCellIds.extend(child["cellIds"])
+        categories = ["sample", "louvain", "metadata", "scratchpad"]
+
+        cell_ids_to_intersect = []
+
+        if (all(len(subset_by[category]) == 0 for category in categories)):
+            pass
+            # TODO: return all_cells
+
+        for category in categories:
+            if (len(subset_by[category])):
+                continue
+
+            cell_ids = get_cell_ids(subset_by[category], category, cell_sets)
+
+            cell_ids_to_intersect.append(cell_ids)
+
+        cell_ids = cell_ids_to_intersect[0].intersection(*cell_ids_to_intersect[1:])
 
         return {
-            "filterBy": list(set(filterByCellIds)),
-            "applyFilter": applyFilter,
+            "subsetBy": list(cell_ids)
         }
+        # if not apply_filter:
+        #     return {
+        #     "filterBy": [],
+        #     "applyFilter": apply_filter,
+        # }
+
+        # # Getting cell ids to subset the seurat object with a group of cells
+        # cellSets = get_cell_sets(self.experiment_id) 
+        # filterByCellSets = []
+        # for cellSet in cellSets:
+        #     if cellSet["key"] in subset_by["group"]:
+        #         filterByCellSets.append(cellSet)
+
+        # filterByCellIds = []
+        # for cellSet in filterByCellSets:
+        #     for child in cellSet["children"]:
+        #         if child["key"] in subset_by["key"]:
+        #             filterByCellIds.extend(child["cellIds"])
+
+        # return {
+        #     "filterBy": list(set(filterByCellIds)),
+        #     "applyFilter": apply_filter,
+        # }
 
 
     @xray_recorder.capture("GetNormalizedExpression.compute")
--- r/R/normalized_matrix.R
+++ r/R/normalized_matrix.R
@@ -17,8 +17,7 @@
 #' @export
 #'
 GetNormalizedExpression <- function(req, data) {
-  apply_filter <- req$body$applyFilter
-  subset_ids <- req$body$filterBy
+  subset_ids <- req$body$subsetBy
 
   if (length(subset_ids) == 0) {
     stop(
@@ -32,9 +31,8 @@ GetNormalizedExpression <- function(req, data) {
   message("Extracting normalized expression matrix")
   message("Number of cells before subsetting: ", ncol(data))
 
-  if (apply_filter == TRUE) {
-    data <- subsetIds(data, cells_id = subset_ids)
-  }
+  data <- subsetIds(data, cells_id = subset_ids)
+  
   norm_matrix <- as.data.frame(Seurat::GetAssayData(data, slot = "data", assay = "RNA"))
 
   message("Number of cells after subsetting: ", ncol(norm_matrix))
