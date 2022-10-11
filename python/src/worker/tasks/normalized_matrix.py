e2131a21c4ebb38435f51bd5a3b69f6aa7fb13be
cosa65 <martin@biomage.net>
2022-10-11 11:32:53 -0300

    Some fixes

--- python/src/worker/tasks/normalized_matrix.py
+++ python/src/worker/tasks/normalized_matrix.py
@@ -11,16 +11,24 @@ from ..helpers.s3 import get_cell_sets
 from ..result import Result
 from ..tasks import Task
 
-# If keys_to_subset
-def get_cell_ids(category, cell_sets, keys_to_subset = []):
-    # Get all cell sets in the category
-    cell_sets_in_category = next(cell_class for cell_class in cell_sets if cell_class["key"] == category)["children"]
 
+def get_cell_sets_dict(cell_sets):
+    cell_sets_dict = {}
+
+    for cell_class in cell_sets:
+        for cell_set in cell_class["children"]:
+            cell_sets_dict[cell_set["key"]] = cell_set["cellIds"]
+
+    return cell_sets_dict
+
+def get_cell_ids(subset_keys, cell_sets_dict):
+    # Get all cell sets in the category
     cell_ids = set()
 
-    for cell_set in cell_sets_in_category:
-        if (keys_to_subset == True or cell_set["key"] in keys_to_subset):
-            cell_ids = cell_ids.union(set(cell_set["cellIds"]))
+    for subset_key in subset_keys:
+        cell_ids = cell_ids.union(set(cell_sets_dict[subset_key]))
+    
+    return cell_ids
 
 
 class GetNormalizedExpression(Task):
@@ -36,12 +44,6 @@ class GetNormalizedExpression(Task):
 
     def _format_request(self):
         subset_by = self.task_def["subsetBy"]
-        # apply_filter = all(group.lower() != "all" for group in subset_by["group"])
-
-        # sample: [],
-        # louvain: [],
-        # metadata: [],
-        # scratchpad: [],
 
         cell_sets = get_cell_sets(self.experiment_id)
 
@@ -50,46 +52,21 @@ class GetNormalizedExpression(Task):
         cell_ids_to_intersect = []
 
         if (all(len(subset_by[category]) == 0 for category in categories)):
-            pass
-            # TODO: return all_cells
+            return { "subsetBy": None }
+
+        cell_sets_dict = get_cell_sets_dict(cell_sets)
 
         for category in categories:
-            if (len(subset_by[category])):
+            if (len(subset_by[category]) == 0):
                 continue
 
-            cell_ids = get_cell_ids(subset_by[category], category, cell_sets)
+            cell_ids = get_cell_ids(subset_by[category], cell_sets_dict)
 
             cell_ids_to_intersect.append(cell_ids)
 
         cell_ids = cell_ids_to_intersect[0].intersection(*cell_ids_to_intersect[1:])
 
-        return {
-            "subsetBy": list(cell_ids)
-        }
-        # if not apply_filter:
-        #     return {
-        #     "filterBy": [],
-        #     "applyFilter": apply_filter,
-        # }
-
-        # # Getting cell ids to subset the seurat object with a group of cells
-        # cellSets = get_cell_sets(self.experiment_id) 
-        # filterByCellSets = []
-        # for cellSet in cellSets:
-        #     if cellSet["key"] in subset_by["group"]:
-        #         filterByCellSets.append(cellSet)
-
-        # filterByCellIds = []
-        # for cellSet in filterByCellSets:
-        #     for child in cellSet["children"]:
-        #         if child["key"] in subset_by["key"]:
-        #             filterByCellIds.extend(child["cellIds"])
-
-        # return {
-        #     "filterBy": list(set(filterByCellIds)),
-        #     "applyFilter": apply_filter,
-        # }
-
+        return { "subsetBy": list(cell_ids) }
 
     @xray_recorder.capture("GetNormalizedExpression.compute")
     @backoff.on_exception(
