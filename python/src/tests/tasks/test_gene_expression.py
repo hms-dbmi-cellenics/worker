103f21b36f682eabde345ea16818b0edd0ad6c3c
cosa65 <martin@biomage.net>
2023-11-10 15:29:59 -0300

    Fix tsts
    Signed-off-by: cosa65 <martin@biomage.net>

--- python/src/tests/tasks/test_gene_expression.py
+++ python/src/tests/tasks/test_gene_expression.py
@@ -13,21 +13,21 @@ class TestGeneExpression:
     @pytest.fixture(autouse=True)
     def load_correct_definition(self):
         self.correct_one_gene = {
-            "downsampled": False,
             "experimentId": "e52b39624588791a7889e39c617f669e",
             "timeout": "2099-12-31 00:00:00",
             "body": {
                 "name": "GeneExpression",
                 "genes": ["Tpt1"],
+                "downsampled": False,
             },
         }
         self.correct_request = {
-            "downsampled": True,
             "experimentId": "e52b39624588791a7889e39c617f669e",
             "timeout": "2099-12-31 00:00:00",
             "body": {
                 "name": "GeneExpression",
                 "genes": ["Tpt1", "Zzz3"],
+                "downsampled": False,
             },
         }
 
