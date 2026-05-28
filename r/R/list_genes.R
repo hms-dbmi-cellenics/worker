86f236ac9c4bf641cdfea16a061a0b1737ed79a9
Alex Pickering <alexvpickering@gmail.com>
2026-05-28 11:12:53 -0400

    make list_genes backward compatible with introduced bug
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- r/R/list_genes.R
+++ r/R/list_genes.R
@@ -1,28 +1,42 @@
+# req has body with:
+# {'selectFields'= {"gene_names", "dispersions"},
+#  'orderBy'= {"gene_names", "dispersions"},
+#  'geneNamesFilter' = string with r placeholders
+#                      (^ before for endswith and $ after for startswith),
+#  'orderDirection'= {DESC, ASC}
+#  'offset'= int: number of genes from the first one in the sorted and 
+#           filtered dataframe to offset the results by
+#  'limit'= int: how many results per page.
 #
+# returns: df with variance.standarized, SYMBOL (gene_names) and full_count
+# collumn with number of genes after filtering.
 #
-# req has body with        {'selectFields'= {"gene_names", "dispersions"},
-#                          'orderBy'= {"gene_names", "dispersions"},
-#                          'geneNamesFilter' = string with r placeholders (^ before for endswith and $ after for startswith),
-#                          'orderDirection'= {DESC, ASC}
-#                          'offset'= int: number of genes from the first one in the sorted and filtered dataframe to offset the results by
-#                          'limit'= int: how many results per page.
+# We decided to return variance.standarized because it's a commonly
+# used indicator of dispersion that accounts for the mean.[1]
 #
-# returns: df with variance.standarized, SYMBOL (gene_names) and full_count collumn with number of genes after filtering.
-#
-# We decided to return variance.standarized because it's a commonly used indicator of dispersion that accounts for the mean.[1]
-#
-# [1]Stuart, T., Butler, A., Hoffman, P., Hafemeister, C., Papalexi, E., Mauck III, W. M., ... & Satija, R. (2019). Comprehensive integration of single-cell data. Cell, 177(7), 1888-1902.
+# [1]Stuart, T., Butler, A., Hoffman, P., Hafemeister, C., Papalexi, E.,
+#   Mauck III, W. M., ... & Satija, R. (2019). Comprehensive integration
+#   of single-cell data. Cell, 177(7), 1888-1902.
 #
 #' @export
 getList <- function(req, data) {
-  select_fields <- req$body$selectFields
   order_by <- req$body$orderBy
   order_decreasing <- req$body$orderDirection == "DESC"
   offset <- req$body$offset
   limit <- req$body$limit
 
-  # Gene dispersion slot generated in data ingest script with the same info as the meta.features slot but with the annotated genes
+  # Gene dispersion slot generated in data ingest script with
+  # the same info as the meta.features slot but with the annotated genes
   gene_results <- data@misc$gene_dispersion
+
+  # workaround for previous bug where HVFInfo now includes variance.expected
+  if (identical(
+    colnames(gene_results)[3:4],
+    c("variance.standardized", NA)
+  )) {
+    colnames(gene_results)[3:4] <-
+      c("variance.expected", "variance.standardized")
+  }
 
   # for "sct" dispersion are residual_variance
   # for "vst" dispersion are variance.standardized
@@ -38,12 +52,20 @@ getList <- function(req, data) {
   # apply gene name filter
   gene_pattern <- req$body$geneNamesFilter
   if (!is.null(gene_pattern)) {
-    gene_filter <- list(list(columnName = "gene_names", expression = gene_pattern))
+    gene_filter <- list(list(
+      columnName = "gene_names",
+      expression = gene_pattern
+    ))
     gene_results <- applyFilters(gene_results, gene_filter)
   }
 
-  paginated_results <- handlePagination(gene_results, offset, limit, order_by, order_decreasing)
+  paginated_results <- handlePagination(
+    gene_results, offset, limit, order_by, order_decreasing
+  )
   gene_results <- paginated_results$gene_results
 
-  return(list(gene_results = gene_results, full_count = paginated_results$full_count))
+  list(
+    gene_results = gene_results,
+    full_count = paginated_results$full_count
+  )
 }
